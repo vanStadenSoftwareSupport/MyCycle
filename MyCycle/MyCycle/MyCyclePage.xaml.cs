ff7a0dfadca57128d67f355498eb60318cb4ce6c
Grant van Staden <grantv@youi.email>
2017-11-16 13:34:28 +1000

    Added initial Xamarin project

---
+++ MyCycle/MyCycle/MyCyclePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MyCycle
+{
+    public partial class MyCyclePage : ContentPage
+    {
+        public MyCyclePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
